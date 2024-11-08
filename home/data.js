128b8ae8310a05ea1a6afc6457245fef4caf683b
Lansky1228 <125549069+Lansky1228@users.noreply.github.com>
2024-11-08 10:49:14 +0800

    A

---
+++ home/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "天橋立",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.5696599711812809,
+        "pitch": 0.014885117026860684,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
